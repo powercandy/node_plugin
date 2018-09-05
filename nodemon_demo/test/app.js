91d35d24e5aa0c7cfd03d4eeae4fa0e9de9627dd
powercandy <13697136064@139.com>
2018-09-05 17:17:24 +0800

    添加服务监听

---
+++ nodemon_demo/test/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+})
+const server = app.listen(3001, function () {
+	console.log('server has beginning!')
+})
